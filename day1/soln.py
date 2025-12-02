b23dbdf4ead929d795e6ad158c4d5b1b33767c36
Younseo+Royce <royceang96@gmail.com>
2025-12-02 15:04:48 -0500

    added changes

--- day1/soln.py
+++ day1/soln.py
@@ -12,6 +12,37 @@ from constants import CONSTANT_PATH, UTILS_PATH, DAY_1_INPUT_PATH
 from utils import load_file_from_input_path
 
 
+def split_command(command: str) -> list[str, str]:
+    '''
+    splits the command into
+    (1) direction and (2) amount
+    e.g. L50
+    [L, 50]
+    '''
+    res: list = []
+    return res[command[0], command[1::]]
+
 # Universal Import Functions
-day_1_inputs = load_file_from_input_path(DAY_1_INPUT_PATH)
-print(day_1_inputs)
+day_1_inputs: list[str] = load_file_from_input_path(DAY_1_INPUT_PATH)
+
+# ranges from 0 - 99
+total_zeroes: int = 0
+for command in day_1_inputs:
+    _direction, _amount = split_command(command)
+    opt: str = None
+    lower_direction = _direction.lower()
+    match lower_direction:
+        case 'l':
+            opt = '-'
+        case 'r':
+            opt = '+'
+        case _:
+            print("unknown direction")
+            raise(Exception("Unknown direction provided"))
+    
+    
+
+
+
+
+
