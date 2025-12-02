0f7ceea08649c2b7be5479373fce41ba1b430e8e
Younseo+Royce <royceang96@gmail.com>
2025-12-02 14:51:05 -0500

    update

--- utils/_file_utils.py
+++ utils/_file_utils.py
@@ -6,8 +6,6 @@ PROJECT_ROOT = Path(__file__).resolve().parents[1]
 if str(PROJECT_ROOT) not in sys.path:
     sys.path.insert(0, str(PROJECT_ROOT))
 
-from constants import CONSTANT_PATH, UTILS_PATH
-
 # load file
 def load_file_from_input_path(input_file_path: Path) -> list: 
     '''
