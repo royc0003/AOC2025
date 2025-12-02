5fda51ff4f1e07d2b7e7791eaeb10454cc2a7c03
Younseo+Royce <royceang96@gmail.com>
2025-12-02 18:29:50 -0500

    added day 1 test case

--- constants/constants.py
+++ constants/constants.py
@@ -7,6 +7,7 @@ REPO_ROOT = Path(__file__).resolve().parents[1]
 CONSTANT_PATH = REPO_ROOT / "constants" / "constants.py"
 UTILS_PATH = REPO_ROOT / "utils" / "_file_utils.py"
 DAY_1_INPUT_PATH = REPO_ROOT / "day1" / "input" / "input.txt"
+DAY_1_TEST_CASE_1 = REPO_ROOT / "day1" / "input" / "testcase1.txt"
 __all__ = [
     "REPO_ROOT",
     "CONSTANT_PATH",
