a71e8c379ea4303e5a4ad75399044d64efc69a76
Younseo+Royce <royceang96@gmail.com>
2025-12-02 14:47:25 -0500

    import utils

--- utils/_file_utils.py
+++ utils/_file_utils.py
@@ -1,6 +1,15 @@
 # Universal Functions
+from pathlib import Path
+import sys
 
+PROJECT_ROOT = Path(__file__).resolve().parents[1]
+if str(PROJECT_ROOT) not in sys.path:
+    sys.path.insert(0, str(PROJECT_ROOT))
+
+from constants import CONSTANT_PATH, UTILS_PATH
 
 # load file
 def load_file_from_input_path():
     pass
+
+print('hello')
