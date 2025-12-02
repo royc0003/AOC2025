77a8c8bec0224a2197c5e04877c5a5c93220b48d
Younseo+Royce <royceang96@gmail.com>
2025-12-02 18:54:25 -0500

    fixe dinit

--- constants/__init__.py
+++ constants/__init__.py
@@ -1,3 +1,18 @@
-from .constants import CONSTANT_PATH, REPO_ROOT, UTILS_PATH, DAY_1_INPUT_PATH, DAY_1_TEST_CASE_1
+from .constants import (
+    CONSTANT_PATH,
+    REPO_ROOT,
+    UTILS_PATH,
+    DAY_1_INPUT_PATH,
+    DAY_1_TEST_CASE_1,
+    DAY_2_TEST_CASE_1,
+)
 
-__all__ = ["REPO_ROOT", "CONSTANT_PATH", "UTILS_PATH", "DAY_1_INPUT_PATH", "DAY_1_TEST_CASE_1"]
+__all__ = [
+    "REPO_ROOT",
+    "CONSTANT_PATH",
+    "UTILS_PATH",
+    "DAY_1_INPUT_PATH",
+    "DAY_1_TEST_CASE_1",
+    "DAY_2_TEST_CASE_1",
+    "DAY_2_INPUT_PATH",
+]
