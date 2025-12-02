65986ca8572503e89d10034f6891a3317ccd8fb4
Younseo+Royce <royceang96@gmail.com>
2025-12-02 18:28:46 -0500

    soln

--- day1/soln.py
+++ day1/soln.py
@@ -19,8 +19,8 @@ def split_command(command: str) -> list[str, str]:
     e.g. L50
     [L, 50]
     '''
-    res: list = []
-    return res[str(command[0]), str(command[1:])]
+    res_1, res_2 = command[0], command[1:]
+    return [str(res_1), str(res_2)]
 
 # Universal Import Functions
 day_1_inputs: list[str] = load_file_from_input_path(DAY_1_INPUT_PATH)
@@ -30,6 +30,7 @@ total_zeroes: int = 0
 res = 0
 for command in day_1_inputs:
     _direction, _amount = split_command(command)
+    _amount = int(_amount)
     opt  = 1
     lower_direction = _direction.lower()
     match lower_direction:
@@ -44,8 +45,9 @@ for command in day_1_inputs:
             raise(Exception("Unknown direction provided"))
 
     _amount *= opt  # sets the sign
-    _amount %= 100
+    print(_amount)
     res += _amount
+    res %= 100
     if res == 0:
         total_zeroes += 1
 print(total_zeroes)
